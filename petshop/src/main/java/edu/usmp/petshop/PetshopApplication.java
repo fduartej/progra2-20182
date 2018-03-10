7cbfaceb30eaa9df1a8f754053870a8d3304633f
Clase <Clase@1A-PC30>
2018-03-10 09:51:07 -0500

    Version 1.0.2

--- petshop/src/main/java/edu/usmp/petshop/PetshopApplication.java
+++ petshop/src/main/java/edu/usmp/petshop/PetshopApplication.java
@@ -1,11 +1,10 @@
-package edu.usmp.petshop.petshop;
+package edu.usmp.petshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"edu.usmp.petshop"})
 public class PetshopApplication {
 
 	public static void main(String[] args) {
