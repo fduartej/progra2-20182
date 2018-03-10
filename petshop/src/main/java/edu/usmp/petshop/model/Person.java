6bb203c42075ff136e89dfbeeb94c2e626f1f773
Clase <Clase@1A-PC30>
2018-03-10 09:47:28 -0500

    Version 1.0.1

---
+++ petshop/src/main/java/edu/usmp/petshop/model/Person.java
@@ -0,0 +1,10 @@
+package edu.usmp.petshop.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+}
---
+++ petshop/src/main/java/edu/usmp/petshop/web/PersonController.java
@@ -0,0 +1,24 @@
+package edu.usmp.petshop.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.usmp.petshop.model.Person;
+
+@Controller
+public class PersonController {
+
+	@GetMapping("/new")
+	public String initCreationForm(Model model) {
+		model.addAttribute("person", new Person());
+		return "personForm";
+	}
+	
+	@PostMapping("/new")
+	public String submitForm() {
+		
+		return "resultForm";
+	}
+}
